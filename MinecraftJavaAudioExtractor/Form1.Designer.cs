1e9ab7423a6b5a12af60712bc6dfb050f2b4578c
TirelessMist <emardag@icloud.com>
2025-03-22 21:33:33 -0500

    Add project files.

---
+++ MinecraftJavaAudioExtractor/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MinecraftJavaAudioExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chooseIndexFileButton = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveToFolderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "Open File";
+            // 
+            // chooseIndexFileButton
+            // 
+            this.chooseIndexFileButton.Location = new System.Drawing.Point(97, 12);
+            this.chooseIndexFileButton.Name = "chooseIndexFileButton";
+            this.chooseIndexFileButton.Size = new System.Drawing.Size(200, 23);
+            this.chooseIndexFileButton.TabIndex = 0;
+            this.chooseIndexFileButton.Text = "Choose Index File...";
+            this.chooseIndexFileButton.UseVisualStyleBackColor = true;
+            this.chooseIndexFileButton.Click += new System.EventHandler(this.chooseIndexFileButton_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(97, 97);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(380, 190);
+            this.treeView1.TabIndex = 1;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Choose Save Folder";
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            // 
+            // saveToFolderButton
+            // 
+            this.saveToFolderButton.Location = new System.Drawing.Point(97, 350);
+            this.saveToFolderButton.Name = "saveToFolderButton";
+            this.saveToFolderButton.Size = new System.Drawing.Size(114, 23);
+            this.saveToFolderButton.TabIndex = 0;
+            this.saveToFolderButton.Text = "Save To Folder...";
+            this.saveToFolderButton.UseVisualStyleBackColor = true;
+            this.saveToFolderButton.Click += new System.EventHandler(this.saveToFolderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.saveToFolderButton);
+            this.Controls.Add(this.chooseIndexFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button chooseIndexFileButton;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button saveToFolderButton;
+    }
+}
+
---
+++ MinecraftJavaAudioExtractor/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Using a index file (selected by user) in the .minecraft > assets > indexes folder, this
+// program extracts all the audio files from the
+// .minecraft > assets > objects folder and saves them in a new folder.
+
+namespace MinecraftJavaAudioExtractor
+{
+    public partial class Form1 : Form
+    {
+        public struct AudioFile
+        {
+            public string path;
+            public string hash;
+            public string size;
+            public AudioFile(string hash, string path, string size)
+            {
+                this.hash = hash;
+                this.path = path;
+                this.size = size;
+            }
+        }
+        AudioFile[] audioFiles;
+        string indexFilePath;
+        string saveToPath;
+        public Form1()
+        {
+            if (Directory.Exists(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\.minecraft\assets\indexes"))
+            {
+                indexFilePath = "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\.minecraft\\assets\\indexes";
+            }
+            else
+            {
+                Console.WriteLine("Warning: could not find default \".minecraft\" folder.");
+            }
+            InitializeComponent();
+        }
+
+        private void chooseIndexFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Index files (*.json)|*.json";
+            openFileDialog1.InitialDirectory = indexFilePath;
+            openFileDialog1.ShowDialog();
+            indexFilePath = openFileDialog1.FileName; // FileName returns entire path, not just the file name
+            Console.WriteLine("Index file path is set to: " + indexFilePath);
+            LoadIndexFile(indexFilePath);
+        }
+
+        private void LoadIndexFile(string indexFilePath)
+        {
+            string contents = File.ReadAllText(indexFilePath).Replace(" ", ""); // entire file as string
+            contents = contents.Remove(0, contents.IndexOf("sounds.json")); // removes everything before "sounds.json" because they are references to languages, built-in packs, etc.
+            string[] entries = contents.Split(new string[] { "}," }, StringSplitOptions.None); // split by "}," to get each audio file AudioFile data structure
+            audioFiles = new AudioFile[entries.Length];
+
+            string[] tempPaths = new string[entries.Length];
+            string[] tempHashes = new string[entries.Length];
+            string[] tempSizes = new string[entries.Length];
+            for (int i = 0; i < entries.Length; i++)
+            {
+                tempPaths[i] = entries[i].Split(new string[] { "\"" }, StringSplitOptions.None)[1]; // get text after first '"' to get path (which is index 1)
+
+                tempHashes[i] = entries[i].Split(new string[] { "\"hash\":\"", "\"" }, StringSplitOptions.None)[3]; // get text after first '"hash":"' to get hash (which is index 3)
+                tempSizes[i] = entries[i].Split(new string[] { "\"size\":", "}" }, StringSplitOptions.None)[1]; // get text after first '"size":' to get size (which is index 1)
+            }
+            for (int i = 0; i < entries.Length; i++)
+            {
+                audioFiles[i] = new AudioFile(tempHashes[i], tempPaths[i], tempSizes[i]);
+            }
+
+            // Testing
+            Console.WriteLine(audioFiles[1].path);
+            Console.WriteLine(audioFiles[1].hash);
+            Console.WriteLine(audioFiles[1].size);
+
+            ExtractAudioFiles();
+        }
+
+        private void ExtractAudioFiles()
+        {
+            string objectsPath = "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\.minecraft\\assets\\objects";
+            string outputPath = "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\.minecraft\\assets\\audio_files";
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+            for (int i = 1; i < audioFiles.Length; i++)
+            {
+                string hash = audioFiles[i].hash;
+                string path = audioFiles[i].path;
+                string size = audioFiles[i].size;
+                string firstTwoChars = hash.Substring(0, 2);
+                string lastChars = hash.Substring(2);
+                string objectPath = objectsPath + "\\" + firstTwoChars + "\\" + hash;
+                string outputPathFile = outputPath + "\\" + path;
+                if (!Directory.Exists(outputPath + "\\" + path.Substring(0, path.LastIndexOf("\\"))))
+                {
+                    Directory.CreateDirectory(outputPath + "\\" + path.Substring(0, path.LastIndexOf("\\")));
+                }
+                File.Copy(objectPath, outputPathFile);
+            }
+        }
+
+        private void saveToFolderButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if (folderBrowserDialog1.SelectedPath != "")
+            {
+                saveToPath = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
